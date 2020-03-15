8449487c1437c5262e55f0b21916cf6c874b5ffa
Mike Nezzer <57691487+mnezz1131@users.noreply.github.com>
2020-03-15 10:04:39 -0500

    final push

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Colussus');
+INSERT INTO burgers (name) VALUES ('The Beast');
+INSERT INTO burgers (name) VALUES ('The Blob');
+INSERT INTO burgers (name, devoured) VALUES ('Juggernaut', true);
+INSERT INTO burgers (name, devoured) VALUES ('The Angel', true);
+INSERT INTO burgers (name) VALUES ('Sabretooth');
